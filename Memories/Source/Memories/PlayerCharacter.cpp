18edc1e22250eb8ce1c3de8fb11565292ba0749b
jeremycbrothers <jeremycbrothers@gmail.com>
2018-05-23 07:31:39 -0400

    Added code for wasd movement and camera pitch and yaw. Pitch and Yaw are not functioning right now though

--- Memories/Source/Memories/PlayerCharacter.cpp
+++ Memories/Source/Memories/PlayerCharacter.cpp
@@ -1,14 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PlayerCharacter.h"
-
+#include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/Controller.h"
 
 // Sets default values
 APlayerCharacter::APlayerCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	BaseLookUpRate = 45.0f;
+	BaseTurnRate = 45.0f;
 
+	// Don't rotate when the controller rotates. Let that just affect the camera.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
 }
 
 // Called when the game starts or when spawned
@@ -16,6 +24,41 @@ void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	
+}
+
+void APlayerCharacter::MoveForward(float Value)
+{
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void APlayerCharacter::MoveRight(float Value)
+{
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void APlayerCharacter::LookupRate(float Rate)
+{
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void APlayerCharacter::RotateCamera(float Rate)
+{
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+	UE_LOG(LogTemp, Warning, TEXT("Rotating: %f"), Rate);
 }
 
 // Called every frame
@@ -30,5 +73,10 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	check(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("LookupRate", this, &APlayerCharacter::LookupRate);
+	PlayerInputComponent->BindAxis("RotateCamrea", this, &APlayerCharacter::RotateCamera);
 }
 
--- Memories/Source/Memories/PlayerCharacter.h
+++ Memories/Source/Memories/PlayerCharacter.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Character.h"
 #include "PlayerCharacter.generated.h"
 
-UCLASS()
+UCLASS(config=Game)
 class MEMORIES_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
@@ -19,7 +19,21 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	virtual void MoveForward(float Value);
+
+	virtual void MoveRight(float Value);
+
+	virtual void LookupRate(float Rate);
+
+	virtual void RotateCamera(float Rate);
+
 public:	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseTurnRate = 0.f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseLookUpRate = 0.f;
+
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
