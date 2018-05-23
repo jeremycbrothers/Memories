7fb3dfda74b12bf461b440ae81cde5340101a121
jeremycbrothers <jeremycbrothers@gmail.com>
2018-05-23 18:11:49 -0400

    Removed old project. Now useing the 3rd person template. Especially for testing

--- Memories/Source/MemoriesEditor.Target.cs
+++ Memories/Source/MemoriesEditor.Target.cs
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
@@ -8,7 +8,6 @@ public class MemoriesEditorTarget : TargetRules
 	public MemoriesEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-
-		ExtraModuleNames.AddRange( new string[] { "Memories" } );
+		ExtraModuleNames.Add("Memories");
 	}
 }
